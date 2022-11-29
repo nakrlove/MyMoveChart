58d5bcc9800c8e9ac62f5b862be66451907fc8d4
nakrlove <nakrlove@gmail.com>
2022-11-29 16:29:43 +0900

    master 두번째 소스 커밋

---
+++ MyMoveChart/MovieCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieCell.swift
+//  MyMoveChart
+//
+//  Created by nakrlove on 2022/11/11.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet var title: UILabel!
+    @IBOutlet var opendate: UILabel!
+    @IBOutlet var desc: UILabel!
+    @IBOutlet var rating: UILabel!
+    @IBOutlet var thumbnail: UIImageView!
+}
---
+++ MyMoveChart/MovieVO1.swift
@@ -0,0 +1,17 @@
+//
+//  MovieVO.swift
+//  MyMoveChart
+//
+//  Created by nakrlove on 2022/11/10.
+//
+
+import Foundation
+
+class MovieVO1 {
+    var thumbnail: String?
+    var title: String?
+    var description: String?
+    var detail: String?
+    var opendate: String?
+    var rating: Double?
+}
